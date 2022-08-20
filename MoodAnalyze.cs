9accb19eda9c9517257d46505660d634682ce7de
bhumika1111 <bhoyarpatil@434>
2022-08-20 23:19:39 +0530

    * Check Test Case For Sad Mood *

---
+++ MoodAnalyze.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzerTestCase1
+{
+    public class MoodAnalyze
+    {
+        private string message;
+        public MoodAnalyze(string message)
+        {
+            this.message = message;
+        }
+        
+        public string AnalyzeMood(string message)
+        {
+            if (this.message.Contains("sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
