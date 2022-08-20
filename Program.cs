3b9976d39af0f9a119fdba27f65d86074e28bde6
bhumika1111 <bhoyarpatil@434>
2022-08-20 23:07:42 +0530

    *Added Mood Analyzer Exception Program File *

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MoodAnalyzerRespond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(":::::Welcome To Mood Analyzer Program:::::::");
+            Console.WriteLine("***Please Enter Which Mood You Have E.g- Happy Or Sad***");
+            string message = Console.ReadLine();
+            // Create Instance Of A class
+            MoodAnalyze check = new MoodAnalyze(message);
+
+            //referance is stored in result
+            string result = check.AnalyseMood(message);
+            Console.WriteLine(result + "Mood");
+        }
+
+    }
+    
+}
