581c988c9b9363199f9b6c48887e4426d126d71f
bhumika1111 <bhoyarpatil@434>
2022-08-21 11:08:06 +0530

    * Handle Exception Test Case Returns Happy *

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodExceptionTestCases
+{
+    public class MoodAnalyzer
+    {
+
+        private string message;
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood(string message)
+        {
+
+            if (message.ToLower().Contains("sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
