1234a41765234a0ede278b5e57f99fb73ae16c35
bhumika1111 <bhoyarpatil@434>
2022-08-21 11:09:23 +0530

    * Handle Exception Test Case File *

---
+++ UnitTest1.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodExceptionTestCases;
+using System;
+
+namespace ExceptionMoodTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        public void CheckInvalidCase()
+        {
+            try
+            {
+                //Arrange
+                string message = "Null";
+
+                //referance
+                MoodAnalyzer moodAnalyze = new MoodAnalyzer(message);
+
+                //Act 
+                string result = moodAnalyze.AnalyzeMood(message);
+
+            }
+
+            catch (NullReferenceException e)
+            {
+                //Assert
+
+                Assert.AreEqual("Happy", e.Message);
+
+
+            }
+        }
+    }
+}
