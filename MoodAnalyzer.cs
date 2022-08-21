916ebfe8a7edd97322426b7c874ca0188d42d4dc
bhumika1111 <bhoyarpatil@434>
2022-08-21 10:37:37 +0530

    * Handle Exception *

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionInvalidMood
+{
+    public class MoodAnalyzer
+    {
+
+
+        private string message;
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood(string message)
+        {
+            
+                if (message.ToLower().Contains("sad"))
+                {
+                    return "Sad";
+                }
+                else
+                {
+                    return "Happy";
+                }
+            
+           
+
+        }
+    }
+}
