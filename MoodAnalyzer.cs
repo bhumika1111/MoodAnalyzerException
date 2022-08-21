78201bf3ddd719eced08050a7800b7da66150bdc
bhumika1111 <bhoyarpatil@434>
2022-08-21 14:33:08 +0530

    * Check To Infor User Enter Invalid Mood*

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomException
+{
+    public class MoodAnalyzer
+    {
+        string message;
+
+        /// <summary>
+        /// Parameterized constructor.
+        /// </summary>
+        /// <param name="message"></param>
+        
+        public string AnalyzeMood(string message)
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+
+            catch(NullReferenceException)
+            {
+                throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
+            
+              
+
+        
+
+    }
+}
+    }
